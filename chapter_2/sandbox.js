a64ec4d5e9ac7868f28264e3d044e6a7a26eb3be
ThinThinzarHtet <100688848+ThinThinzar@users.noreply.github.com>
2022-06-04 15:25:42 +0630

    day 2

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,5 +1,5 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
 //log things to console
 // console.log(age, year);
@@ -10,5 +10,81 @@ console.log(age);
 const points = 100;
 console.log(points);*/
 
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
+
+/*--------- Day-02 ------------*/
+//strings
+
+// console.log('hello, world');
+// let email = 'ttzh@gmail.com';
+// console.log(email);
+
+// string concatenation
+// let firstName = 'Thin';
+// let lastName = 'Thinzar';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// getting characters
+// console.log(fullName[2]); //to get a single character
+
+// string length
+// console.log(fullName.length);
+
+// string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLocaleLowerCase(); //these methods did not alter the original ones
+// console.log(result, fullName);
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+// common string methods
+// let email = 'mariho@ttzh.co.uk';
+// let result = email.lastIndexOf('h');
+
+// let result = email.slice(0, 5); //slice(m, n) -> return m to n-1
+
+// let result = email.substr(4, 10); //position 4 to position 14
+
+// let result = email.replace('m', 'w');
+
+// let result = email.replace('h', 'p'); //1st character found ko replace
+// console.log(result);
+
+/*------ Numbers ---------*/
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+//math operators +, -, *, /, **, %
+// console.log(10 / 2);
+
+// let result = radius % 3;
+// let result = pi * radius ** 2;
+
+// order of operation - B I D M A S(brackets, indices**, division, multiplication, addition, substraction)
+// let result = 5 * (10 - 3) ** 2;
+// console.log(result);
+
+let likes = 10;
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes';
+console.log(result);
